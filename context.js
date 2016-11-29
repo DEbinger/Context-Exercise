36ead9bcfb0edb7be798f1018b8ad8459015a9f0
David Ebinger <davidebinger888@gmail.com>
2016-11-28 16:23:49 -1000

    Context-Exercise completed

--- context.js
+++ context.js
@@ -1,30 +1,30 @@
-var Robot = function (name) {
+let Robot = function (name) {
   this.name = name;
 
-}
+};
 
 function add ( op1, op2 ) {
   this.name = this.name || "Humans";
   return this.name + " can count to " + (op1 + op2);
 }
 
-var voltron = new Robot("Voltron");
-var bender = new Robot("Bender Rodriguez");
-var optimus = new Robot("Optimus Prime");
-var megaman = new Robot("Mega Man");
-var bmo = new Robot("B-Mo");
-var wall_e = new Robot("Wall-E");
+let voltron = new Robot("Voltron");
+let bender = new Robot("Bender Rodriguez");
+let optimus = new Robot("Optimus Prime");
+let megaman = new Robot("Mega Man");
+let bmo = new Robot("B-Mo");
+let wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(  );
+console.log(add(0, 1));
 
 // #2  
-console.log(  );
+console.log(add.call(voltron, 2, 3));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus, [20, 30]));
 
 // #4
-
-console.log(  );
+let calculate = add.bind(bender);
+console.log(calculate('drinking', 'beer'));
